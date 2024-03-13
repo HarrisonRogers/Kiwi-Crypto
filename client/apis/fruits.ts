0163830a7d8496d7574be3a558ae1e201fbac75b
HarrisonRogers <harrison.rogers05@gmail.com>
2024-03-14 11:56:11 +1300

    add api key

--- client/apis/fruits.ts
+++ client/apis/fruits.ts
@@ -2,8 +2,8 @@ import request from 'superagent'
 
 const rootUrl = '/api/v1'
 
-export function getFruits(): Promise<string[]> {
-  return request.get(rootUrl + '/fruits').then((res) => {
+export function getCrypto(): Promise<string[]> {
+  return request.get(rootUrl + '/cryptos').then((res) => {
     return res.body.fruits
   })
 }
