055dca28487f517e24541c6b69504949d0953ccb
HarrisonRogers <harrison.rogers05@gmail.com>
2024-03-16 15:28:08 +1300

    add route

--- server/routes/fruits.ts
+++ server/routes/fruits.ts
@@ -1,17 +1,25 @@
-import { Router } from 'express'
+import e, { Router, response } from 'express'
 
 import * as db from '../db/fruits.ts'
+import request from 'superagent'
+import 'dotenv/config'
+
+const apiKey = process.env.CRYPTO_API_KEY
 
 const router = Router()
 
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
   try {
-    const fruits = await db.getAllFruits()
+    const response = await request
+      .get('https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest')
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      .set('X-CMC_PRO_API_KEY', apiKey!)
+      .query({ id: '1' })
 
-    res.json({ fruits: fruits.map((fruit) => fruit.name) })
+    res.json(response.body.data)
   } catch (error) {
     console.log(error)
-    res.status(500).json({ message: 'Something went wrong' })
+    next(error)
   }
 })
 
