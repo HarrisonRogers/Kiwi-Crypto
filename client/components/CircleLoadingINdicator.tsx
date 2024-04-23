8f2c5c7fc5fd397cea81f62adaa3a4cf03eb8a81
HarrisonRogers <harrison.rogers05@gmail.com>
2024-04-23 20:53:22 +1200

    add class to indicator and change file name

--- client/components/CircleLoadingINdicator.tsx
+++ client/components/CircleLoadingINdicator.tsx
@@ -1,6 +1,6 @@
 export default function CircleLoadingIndicator() {
   return (
-    <div className="loading-container">
+    <div className="container loading-container">
       <div className="loader-2"></div>
     </div>
   )
